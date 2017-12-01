8caceb99e99a9dd4532b97fbf3048a04284fba26
augustomatos333 <augusto.matos3@yahoo.com.br>
2017-12-01 01:56:36 -0200

    Estrutura e visual de videos e videoscont melhorada
    Estrutura e visual de videos e videoscont melhorada

--- videoconteudo.php
+++ videoconteudo.php
@@ -2,26 +2,44 @@
 <?php require_once 'portugues.php'; ?>
 <div class="row contentvideo col-lg-9">
     <div>
-    <iframe width="854" height="480" src="https://www.youtube.com/embed/nouCGtRyY-A?ecver=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+    <iframe width="854" height="480" src="https://www.youtube.com/embed/H-xlHSCRra0?ecver=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
     </div>
-   <h4>Top 20 Worst Guns and Weapons in Borderlands 2 #PumaCounts</h4>
-   <div class="row descricao col-lg-7" >
-     <h4>DESCRIÇÃO</h4>
+   <h3>Top 20 Worst Guns and Weapons in Borderlands 2 #PumaCounts</h3>
+   <hr style=" border: 1px solid lightgrey; width: 900px;">
+   <div class=" descricao col-lg-7" >
+     <h3>DESCRIÇÃO</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor felis nec purus eleifend, eu feugiat massa consequat. Etiam hendrerit malesuada massa quis iaculis. In efficitur porttitor varius. In rhoncus placerat porttitor. Etiam porttitor enim a augue lobortis congue. Phasellus id tellus arcu. Praesent feugiat elit nec lorem convallis dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu libero orci. Nullam at diam non turpis vestibulum pretium at ut ligula.</p>
+      <hr style=" border: 1px solid lightgrey; width: 900px;">
+   </div>
+   <div class="coments col-lg-7" >
+       <div class="col-lg-l2">
+         <h3>COMENTARIOS</h3>
+         &nbsp;
+         <h4><i class="fa fa-user fa-lg" aria-hidden="true"></i>&nbsp;&nbsp;Usuario</h4>
+         <p>Vídeo muito bom!</p>
+       </div>
+       &nbsp;
+       <div class="col-lg-l2">
+         <h4><i class="fa fa-user fa-lg" aria-hidden="true"></i>&nbsp;&nbsp;Usuario</h4>
+         <p>Vídeo muito bom!</p>
+       </div>
+       &nbsp;
+       <div class="col-lg-l2">
+         <h4><i class="fa fa-user fa-lg" aria-hidden="true"></i>&nbsp;&nbsp;Usuario</h4>
+         <p>Vídeo muito bom!</p>
+       </div>
+
    </div>
 </div>
-<div class=" relacionados row col-lg-3">
+<div class=" relacionados col-lg-3">
 <?php
-echo '<div class="video1 col-lg-12 col-md-11 col-sm-11 col-xs-11">';
-echo '<h4><center>Relacionados</center></h4>';
-echo '<a href="#"><center><img src="'.$musica[3].'" alt=""><center><h4><center>Música</center></h4></a>';
-echo '</div>';
+  echo '<h4><center>Relacionados<center><h4>';
+  for($i = 0; $i < 9; $i++){
+    if ($i != 0) {
+   echo '<div class="col-lg-12">';
+   echo '<a href="#"><center><img src="'.$jogo[$i].'" alt=""><center><h5><center>'.$titulo[$i].'</center></h5></a>';
+   echo '</div>';
+ }
+}
  ?>
-</div>
-
-<div class="row coments col-lg-7" >
-  <h2>COMENTARIOS</h2>
-  <h5>Usuario</h5>
-  <p><i class="fa fa-user fa-lg" aria-hidden="true"></i>Vídeo muito bom!</p>
-
 </div>
